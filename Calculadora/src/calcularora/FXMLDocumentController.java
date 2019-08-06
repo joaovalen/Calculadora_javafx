76424c4ce7744d5d0eaad30273dbf1d5cea5c69a
05200246 <05200246@D54973.farroupilha.ifrs.edu.br>
2019-08-06 18:49:48 -0300

    a

--- Calculadora/src/calcularora/FXMLDocumentController.java
+++ Calculadora/src/calcularora/FXMLDocumentController.java
@@ -13,6 +13,7 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.input.KeyEvent;
 
 /**
  *
@@ -64,12 +65,8 @@ public class FXMLDocumentController implements Initializable {
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-       
+       tela_operator.setText(null);
     }    
-
-    private void b7a(ActionEvent event) {
-        System.out.println("Clicou 7");
-    }
 
     @FXML
     private void button_zero_pressed(ActionEvent event) {
@@ -161,8 +158,119 @@ public class FXMLDocumentController implements Initializable {
         }
     }
 
-   
-   
+    @FXML
+    private void button_dot_pressed(ActionEvent event) {
+        if (tela_operator.getText() == null){
+            if (tela_first_number.getText().contains(".")){
+            }else{
+                tela_first_number.setText(tela_first_number.getText() + ".");
+            }
+        }else{
+             if (tela_second_number.getText().contains(".")){
+            }else{
+                tela_second_number.setText(tela_second_number.getText() + ".");
+            }
+        }
+    }
+
+    
+    
+    @FXML
+    private void button_result_pressed(ActionEvent event) {
+        float result = operation();
+        tela_operator.setText(null);
+        tela_first_number.setText(null);
+        tela_second_number.setText(null);
+    }
+
+    @FXML
+    private void button_divide_pressed(ActionEvent event) {
+        if (tela_operator.getText() == null){
+            tela_operator.setText("/");
+        }
+    }
+
+    @FXML
+    private void button_minus_pressed(ActionEvent event) {
+        if (tela_operator.getText() == null){
+            tela_operator.setText("-");
+        }
+    }
+
+    @FXML
+    private void button_times_pressed(ActionEvent event) {
+        if (tela_operator.getText() == null){
+            tela_operator.setText("x");
+        }
+    }
+
+    @FXML
+    private void button_plus_pressed(ActionEvent event) {
+        if (tela_operator.getText() == null){
+            tela_operator.setText("+");
+        }
+    }
+    
+    private float operation(){
+        String operator = tela_operator.getText();
+        Float result;
+        
+        switch (operator){
+            case "+": 
+                result = plus_operation();
+                break;
+            case "-":
+                result = minus_operation();
+                break;
+            case "x":
+                result = times_operation();
+                break;
+            case "/": 
+                result = divide_operation();
+                break;
+            default:
+                result = null;
+                break;
+        }
+        return result;
+    }
+    
+    private float plus_operation(){
+        float first_number = Float.parseFloat((tela_first_number.getText()));
+        float second_number = Float.parseFloat((tela_second_number.getText()));
+        
+        float result = first_number + second_number;
+        return result;
+    }
+    
+    private float minus_operation(){
+        float first_number = Float.parseFloat((tela_first_number.getText()));
+        float second_number = Float.parseFloat((tela_second_number.getText()));
+        
+        float result = first_number - second_number;
+        return result;
+    }
+    
+    private float times_operation(){
+        float first_number = Float.parseFloat((tela_first_number.getText()));
+        float second_number = Float.parseFloat((tela_second_number.getText()));
+        
+        float result = first_number * second_number;
+        return result;
+    }
+    
+    private float divide_operation(){
+        float first_number = Float.parseFloat((tela_first_number.getText()));
+        float second_number = Float.parseFloat((tela_second_number.getText()));
+        
+        float result = first_number / second_number;
+        return result;
+    }
+
+    @FXML
+    private void number_click(KeyEvent event) {
+        event.getText();
+    }
     
     
 }
